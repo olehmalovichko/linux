4152212f30f2af9af45ae4013b9ec514d862db92
xxcombat <cs.group.com@gmail.com>
2012-07-20 22:36:27 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@
 mkdir -p  ~/.local/share/gedit/plugins
 cp mencode.plugin mencode.py ~/.local/share/gedit/plugins
 
+# install locale
+su
+cp mencode.mo_ru /usr/share/locale/ru/LC_MESSAGES/mencode.mo
+cp mencode.mo_ua /usr/share/locale/ua/LC_MESSAGES/mencode.mo
+
+
+
+
+
