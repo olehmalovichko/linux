800d291c7675a8f656e271fa02d1ce8ffd25b535
xxcombat <cs.group.com@gmail.com>
2012-07-21 14:00:00 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -1,11 +1,36 @@
-#install mencode
-mkdir -p  ~/.local/share/gedit/plugins
-cp mencode.plugin mencode.py ~/.local/share/gedit/plugins
+#!/bin/bash
 
-# install locale
-su
-cp mencode.mo_ru /usr/share/locale/ru/LC_MESSAGES/mencode.mo
-cp mencode.mo_ua /usr/share/locale/ua/LC_MESSAGES/mencode.mo
+#username
+user="user"
+
+lpath="/home/$user/.local/share/gedit/plugins"
+path_ua="/usr/share/locale/ua/LC_MESSAGES/mencode.mo"
+path_ru="/usr/share/locale/ru/LC_MESSAGES/mencode.mo"
+
+
+if [[ $EUID -ne 0 ]]; then
+ echo "You must be a root user" 2>&1
+ exit 1
+else
+
+ grep "^$user:" /etc/passwd >/dev/null
+ if [ $? -ne 0 ]; then
+  echo "Not found user $user "
+  exit 1
+ else
+  #install mencode
+  mkdir -p $lpath
+  cp mencode.plugin mencode.py $lpath
+  chown -R $user:$user $lpath
+
+  # install locale
+  cp mencode.mo_ru $path_ru
+  cp mencode.mo_ua $path_ua
+  chown root:root $path_ua
+  chown root:root $path_ru
+ fi
+
+fi
 
 
 
